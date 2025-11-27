90bf111323f52b106abfe653d155061f6f7c5f31
Miloslavov Nikita <nakytasama@mail.ru>
2025-11-27 21:23:08 +0300

    Another one

---
+++ src/main/java/org/example/site4/Site4Application.java
@@ -0,0 +1,13 @@
+package org.example.site4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Site4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Site4Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/site4/Site4ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.site4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Site4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
